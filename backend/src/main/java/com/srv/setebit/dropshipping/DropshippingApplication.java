b0e714d843583aa229b13b2b1ab62546cc42d7e8
claudemir <claudemirramosferreira@gmail.com>
2026-02-17 21:07:58 -0300

    ajuste deploy

--- backend/src/main/java/com/srv/setebit/dropshipping/DropshippingApplication.java
+++ backend/src/main/java/com/srv/setebit/dropshipping/DropshippingApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         entityManagerFactoryRef = "entityManagerFactory")
 public class DropshippingApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DropshippingApplication.class, args);
 	}
